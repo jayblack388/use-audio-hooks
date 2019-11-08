b08788ba01aebe8b27170cec8c794b43930fb184
John Blackwell <jblackwell072588@gmail.com>
2019-11-07 17:12:08 -0800

    adjust playground

--- example/index.tsx
+++ example/index.tsx
@@ -4,7 +4,15 @@ import * as ReactDOM from 'react-dom';
 import { Audio } from '../.';
 const App = () => {
   return (
-    <div>
+    <div
+      style={{
+        width: '100vw',
+        height: '60vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
       <Audio />
     </div>
   );
